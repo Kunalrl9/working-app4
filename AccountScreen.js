30b03e3fbfd250b819a7db08797ea6c7d2dd8e0a
Shriyash16 <shirodkarshriyash596@gmail.com>
2024-02-12 21:00:53 -0800

    new

--- AccountScreen.js
+++ AccountScreen.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import * as ImagePicker from 'react-native-image-picker';
+import  ImagePicker from 'react-native-image-picker';
 
 
 const AccountScreen = () => {
@@ -59,66 +58,4 @@ const styles = StyleSheet.create({
   },
 });
 
-=======
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import * as ImagePicker from 'react-native-image-picker';
-
-
-const AccountScreen = () => {
-  const [username, setUsername] = useState('John Doe');
-  const [profilePhoto, setProfilePhoto] = useState('');
-
-  const selectImage = () => {
-    const options = {
-      title: 'Select Profile Photo',
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-
-    ImagePicker.showImagePicker(options, (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else {
-        setProfilePhoto(response.uri);
-      }
-    });
-  };
-
-  return  (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={selectImage}>
-        <Image
-          source={{ uri: profilePhoto }}
-          style={styles.profilePhoto}
-        />
-      </TouchableOpacity>
-      <Text style={styles.username}>{username}</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  profilePhoto: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20,
-  },
-  username: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
-
->>>>>>> 7ff1b58b6c47024b43b0f85f17e4f96d2153d316
-export default AccountScreen
+export default AccountScreen;
